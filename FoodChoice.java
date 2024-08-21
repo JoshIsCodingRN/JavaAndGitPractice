8ade0a849a17bbc4b22f50b6bae6d598bf716714
JoshIsCodingRN <132061175+JoshIsCodingRN@users.noreply.github.com>
2024-08-21 09:39:32 -0400

    added food methods

--- FoodChoice.java
+++ FoodChoice.java
@@ -2,22 +2,47 @@ import java.util.Scanner;
 
 public class FoodChoice
 {
-    private Scanner scanner;
-    private String foodChoice;
-    private int entree;
+    private Scanner scanner = new Scanner(System.in);
+    private int entree = 0;
     private int side;
 
 
     public static void main(String[] args)
     {
+      
+
 
     }
 
     public void promptEntreeChoice()
     {
         System.out.println("What food would you like?");
-        System.out.print("1. burga\n2.hotdawg\n3.chicken sammy");
+        System.out.println("1. burga\n2.hotdawg\n3.chicken sammy");
     }
 
-    
+    public void promptSideChoice()
+    {
+        System.out.println("What side would you like?");
+        System.out.println("1. fries\n2. maccy cheese\n3. onion ring");
+    }
+
+    public int scanEntree()
+    {
+        while (entree < 3 || entree > 3)
+        {
+           entree = scanner.nextInt();
+        }
+
+        return entree;
+    }
+
+    public int scanSide()
+    {
+        while (side < 3 || side > 3)
+        {
+           side  = scanner.nextInt();
+        }
+
+        return side;
+    }
 }

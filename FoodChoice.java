15729814e0f84bcae63cdb804e0afc936492b14f
JoshIsCodingRN <132061175+JoshIsCodingRN@users.noreply.github.com>
2024-08-21 09:21:39 -0400

    first commit

---
+++ FoodChoice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FoodChoice
+{
+    private Scanner scanner;
+    private String foodChoice;
+    private int entree;
+    private int side;
+
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public void promptEntreeChoice()
+    {
+        System.out.println("What food would you like?");
+        System.out.print("1. burga\n2.hotdawg\n3.chicken sammy");
+    }
+
+    
+}

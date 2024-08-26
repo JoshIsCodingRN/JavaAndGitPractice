3b83e97f2fc304c13130cf514aeebfceecc19871
JoshIsCodingRN <132061175+JoshIsCodingRN@users.noreply.github.com>
2024-08-25 23:40:56 -0400

    finished main added documentation

--- DessertChoice.java
+++ DessertChoice.java
@@ -1,36 +1,59 @@
 import java.util.Scanner;
 
+/**
+ * class that gives you choices for dessert and 
+ * what utinsil you would like to use
+ * 
+ * @author Josh Howard
+ */
 public class DessertChoice implements ItemInfo
 {
+    //fields
     private Scanner scanner = new Scanner(System.in);
     private int dessert = 0;
     private int utensil = 0;
     private int utinsil;
 
 
+    /**
+     * main method for testing
+     * @param args
+     */
     public static void main(String[] args)
     {
-        DessertChoice object = new DessertChoice();
+        /*     
+         DessertChoice object = new DessertChoice();
 
         object.promptDessertChoice();
         object.scanDessert();
 
         object.promptUtinsilChoice();
         object.scanUtinsil();
+         */
     }
 
+    /**
+     * asks what dessert and lists options
+     */
     public void promptDessertChoice()
     {
         System.out.println("What dessert would you like?");
         System.out.println("1. cake\n2. ice cream\n3.brownie");
     }
 
+    /**
+     * asks what utinsil and gives options
+     */
     public void promptUtinsilChoice()
     {
         System.out.println("What utensil would you like?");
         System.out.println("1. spoon\n2. fork\n3. spork");
     }
 
+    /**
+     * scans for dessert option
+     * @return dessert option
+     */
     public int scanDessert()
     {
         while (dessert < 3 || dessert > 3)
@@ -41,6 +64,10 @@ public class DessertChoice implements ItemInfo
         return dessert;
     }
 
+    /**
+     * scans for utinsil option
+     * @return utinsil option
+     */
     public int scanUtinsil()
     {
         while (utensil < 3 || utensil > 3)
@@ -51,14 +78,47 @@ public class DessertChoice implements ItemInfo
         return utensil;
     }
 
+    /**
+     * getter
+     * @return dessert option
+     */
     public int getDessert()
     {
         return dessert;
     }
 
+    /**
+     * getter
+     * @return utinsil option
+     */
     public int getUtinsil()
     {
         return utinsil;
+    }
+
+    /**
+     * method used to display calories of dessert choice 
+     * utilizing a switch.
+     */
+    @Override
+    public void calories() {
+        switch (dessert) {
+            case 1:
+                System.out.println("15"); 
+                break;
+        
+            case 2:
+                System.out.println("20");
+                break;
+                        
+            case 3:
+                System.out.println("25");
+                break;
+                    
+            default:
+                System.out.println("no dessert selected");
+                break;
+        }
     }
 
 
--- FoodChoice.java
+++ FoodChoice.java
@@ -1,35 +1,55 @@
 import java.util.Scanner;
 
-public class FoodChoice
+/**
+ * class for choosing food and side
+ * 
+ * @author Josh Howard
+ */
+public class FoodChoice implements ItemInfo
 {
+    //fields
     private Scanner scanner = new Scanner(System.in);
     private int entree = 0;
     private int side = 0;
 
 
+    /**
+     * main method for testing.
+     * @param args string arguments
+     */
     public static void main(String[] args)
     {
-        FoodChoice object = new FoodChoice();
+        // FoodChoice object = new FoodChoice();
 
-        object.promptEntreeChoice();
-        object.scanEntree();
+        // object.promptEntreeChoice();
+        // object.scanEntree();
 
-        object.promptSideChoice();
-        object.scanSide();
+        // object.promptSideChoice();
+        // object.scanSide();
     }
 
+    /**
+     * prompts user for entree choice and gives options
+     */
     public void promptEntreeChoice()
     {
         System.out.println("What food would you like?");
         System.out.println("1. burga\n2.hotdawg\n3.chicken sammy");
     }
 
+    /**
+     * prompts user for side choice and gets options
+     */
     public void promptSideChoice()
     {
         System.out.println("What side would you like?");
         System.out.println("1. fries\n2. maccy cheese\n3. onion ring");
     }
 
+    /**
+     * scans for user entree input
+     * @return entree choice
+     */
     public int scanEntree()
     {
         while (entree < 3 || entree > 3)
@@ -40,6 +60,10 @@ public class FoodChoice
         return entree;
     }
 
+    /**
+     * scans for user side input
+     * @return side choice
+     */
     public int scanSide()
     {
         while (side < 3 || side > 3)
@@ -48,5 +72,29 @@ public class FoodChoice
         }
 
         return side;
+    }
+
+    /**
+     * interface method for displaying calories of entree
+     */
+    @Override
+    public void calories() 
+    {
+      if (entree == 0)
+      {
+        System.out.println("no entree chosen");
+      } 
+      else if(entree == 1)
+      {
+        System.out.println("20");
+      }
+      else if(entree == 2)
+      {
+        System.out.println("30");
+      }
+      else if(entree == 3)
+      {
+        System.out.println("40");
+      }
     }
 }
